774fc79d429fc37bede6517190e2dfe605166b13
Shenzhen YAO <junk.them@hotmail.com>
2021-09-03 11:22:43 -0600

    20210903a

--- test.js
+++ test.js
@@ -8,6 +8,9 @@ var $ = require("jquery")(window);
 const srcfolder = 'data/in/prototype/__xml/egpv8/';
 
 (async () => { 
-
+    let thesrcxmlfile = 'data/in/prototype/__xml/egpv7/__egtask_example.xml'
+    let encoding = "utf-8"; // the srcxml is directly from an egp file, remmember to read in using "utf16le" encoding
+    let thesrcxmlstr = await mymodules.readtxt(thesrcxmlfile, encoding);
+    console.log(thesrcxmlstr)
 })()
 
